6452ff052b04d367fdf16de34f6529bf2a8f37f3
Pelegrinetti <pelegrinetti.dev@gmail.com>
2020-05-20 20:20:39 -0300

    add sensor activity

---
+++ app/src/main/java/com/example/sensores/SensorActivity.java
@@ -0,0 +1,13 @@
+package com.example.sensores;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SensorActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sensor);
+    }
+}
