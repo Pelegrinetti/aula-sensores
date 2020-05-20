fa02c949924baedc749ebbc07efdc12a3eb0f06a
Pelegrinetti <pelegrinetti.dev@gmail.com>
2020-05-20 20:22:12 -0300

    add sensors list

--- app/src/main/java/com/example/sensores/MainActivity.java
+++ app/src/main/java/com/example/sensores/MainActivity.java
@@ -2,13 +2,51 @@ package com.example.sensores;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.hardware.Sensor;
+import android.hardware.SensorManager;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+import java.util.ArrayList;
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity implements AdapterView.OnItemClickListener {
+    private SensorManager sensorManager;
+    private List<Sensor> sensorList;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        sensorManager = (SensorManager) getSystemService(SENSOR_SERVICE);
+        sensorList = sensorManager.getSensorList(Sensor.TYPE_ALL);
+
+        List<String> names = new ArrayList<String>();
+
+        for (Sensor s : sensorList) {
+            names.add(s.getName() + " - " + s.getVendor() + " - " + s.getType());
+        }
+
+        ListView listView = (ListView) findViewById(R.id.sensorsList);
+        listView.setOnItemClickListener(this);
+        int layout = android.R.layout.simple_list_item_1;
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, layout, names);
+        listView.setAdapter(adapter);
+    }
+
+    @Override
+    public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+        Sensor sensor = sensorList.get(i);
+        String msg = sensor.getName() + " - " + sensor.getVendor();
+        Toast.makeText(getApplicationContext(), msg, Toast.LENGTH_SHORT).show();
+        Intent intent = new Intent(this, SensorActivity.class);
+        intent.putExtra("position", i);
+        startActivity(intent);
     }
 }
--- app/src/main/java/com/example/sensores/SensorActivity.java
+++ app/src/main/java/com/example/sensores/SensorActivity.java
@@ -1,13 +1,49 @@
 package com.example.sensores;
 
 import androidx.appcompat.app.AppCompatActivity;
-import android.os.Bundle;
 
-public class SensorActivity extends AppCompatActivity {
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
+import android.os.Bundle;
+import android.widget.TextView;
+
+import java.util.List;
+
+public class SensorActivity extends AppCompatActivity implements SensorEventListener {
+    private SensorManager sensorManager;
+    private Sensor sensor;
+    private TextView sensorValue;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sensor);
+
+        int position = getIntent().getIntExtra("position", 0);
+
+        sensorManager = (SensorManager) getSystemService(SENSOR_SERVICE);
+        List<Sensor> sensorList = sensorManager.getSensorList(Sensor.TYPE_ALL);
+        sensor = sensorList.get(position);
+        sensorValue = (TextView) findViewById(R.id.sensorValue);
+        super.onResume();
+        sensorManager.registerListener(this, sensor, SensorManager.SENSOR_DELAY_NORMAL);
+    }
+
+    @Override
+    public void onSensorChanged(SensorEvent event) {
+        StringBuffer sb = new StringBuffer();
+
+        for (int i = 0; i < event.values.length; i++) {
+            sb.append(i).append(": ").append(event.values[i]).append("\n");
+        }
+
+        sensorValue.setText(sb.toString());
+    }
+
+    @Override
+    public void onAccuracyChanged(Sensor sensor, int accuracy) {
+        // code
     }
 }
